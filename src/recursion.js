70342ae03a75fa8318a6a646ec29ec6cfe105aa2
Bernie <smokeinthedesert@gmail.com>
2025-11-16 20:38:49 -0600

    recursion update 11/16/2025

--- src/recursion.js
+++ src/recursion.js
@@ -4,13 +4,24 @@
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5);  // 120
-var factorial = function(n) {
- 
-};
+function factorial(n) {
+  if (n === 0) {
+    return 1;
+  }else if (n < 0){
+    return null;
+  }
 
+  return n * factorial(n - 1);
+}
 // 2. Compute the sum of an array of integers.
 // Example:  sum([1, 2, 3, 4, 5, 6]);  // 21
 var sum = function(array) {
+  let total = 0;
+  if (array.length === 0){
+    return total;
+  }
+  total += array[0];  
+  return total + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
@@ -19,18 +30,48 @@ var arraySum = function(array) {
 };
 
 // 4. Check if a number is even.
-var isEven = function(n) {
-};
+function isEven(n) {
+  if (n === 0) {
+    return true;
+  } else if (n === 1) {
+    return false;
+  } else if (n < 0) {
+    return isEven(-n);
+  } else {
+    return isEven(n - 2);
+  }
+}
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  let sum = 0;
+  if (n === 0){
+    return sum;
+  }
+  if (n > 0){
+    sum += (n - 1);
+    return sum + sumBelow(n - 1);
+  }else if (n < 0){
+    sum += (n + 1);
+    return sum + sumBelow(n + 1);
+  }
 };
 
 // 6. Get the integers in range (x, y).
 // Example:  range(2, 9);  // [3, 4, 5, 6, 7, 8]
 var range = function(x, y) {
+  let numbers = [];
+  if (x < y){
+    if(gitx === y){
+      return numbers;
+    }else {
+      numbers.push(x);
+      return range(x + 1, y);
+    }
+  }
+  return numbers;
 };
 
 // 7. Compute the exponent of a number.
@@ -46,6 +87,9 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  if (n === 0){
+
+  }
 };
 
 // 9. Write a function that accepts a string a reverses it.
